728a6b561cf1436ede2df5e2f03a19ef84d1df76
DCoded <dicoded@email.com>
2021-03-15 21:59:45 -0400

    Added imports for rest of files in /slave/desc

--- src/routes/slaves/View.svelte
+++ src/routes/slaves/View.svelte
@@ -2,17 +2,29 @@
   import Slave from '../../ts/classes/slave/Slave';
 
   import Header from '../../components/ui/Header.svelte';
-  import devotion, { devotionColor } from '../../ts/util/slave/desc/devotion';
-  import trust, { trustColor } from '../../ts/util/slave/desc/trust';
-  import name from '../../ts/util/slave/desc/name';
+
   import age from '../../ts/util/slave/desc/age';
-  import health, { healthColor } from '../../ts/util/slave/desc/health';
-  import mind from '../../ts/util/slave/desc/mind';
-  import relationRival from '../../ts/util/slave/desc/relationRival';
+  import armwear from '../../ts/util/slave/desc/armwear';
   import career from '../../ts/util/slave/desc/career';
+  import clothing from '../../ts/util/slave/desc/clothing';
+  import devotion, { devotionColor } from '../../ts/util/slave/desc/devotion';
+  import eyes from '../../ts/util/slave/desc/eyes';
+  import family from '../../ts/util/slave/desc/family';
+  import health, { healthColor } from '../../ts/util/slave/desc/health';
+  import legs from '../../ts/util/slave/desc/legs';
+  import limbs from '../../ts/util/slave/desc/limbs';
+  import mind from '../../ts/util/slave/desc/mind';
+  import name from '../../ts/util/slave/desc/name';
+  import nationality from '../../ts/util/slave/desc/nationality';
+  import piercings from '../../ts/util/slave/desc/piercings';
+  import pregnancy from '../../ts/util/slave/desc/pregnancy';
   import prestige from '../../ts/util/slave/desc/prestige';
+  import relationRival from '../../ts/util/slave/desc/relationRival';
+  import score from '../../ts/util/slave/desc/score';
   import size from '../../ts/util/slave/desc/size';
+  import skills from '../../ts/util/slave/desc/skills';
   import skin from '../../ts/util/slave/desc/skin';
+  import trust, { trustColor } from '../../ts/util/slave/desc/trust';
 
   import activeSlave from '../../stores/active.store';
 
