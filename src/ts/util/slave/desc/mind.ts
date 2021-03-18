e00c26286c1eb332ea1dc7e1043ea7b253d910f2
DCoded <dicoded@email.com>
2021-03-17 21:46:49 -0400

    Refactored attraction()

--- src/ts/util/slave/desc/mind.ts
+++ src/ts/util/slave/desc/mind.ts
@@ -10,82 +10,117 @@ import { Sex } from '../../../classes/body/nonphysical/Nonphysical';
 
 const PC = get(player);
 
+function getDrive(slave: Slave): string {
+  const { He } = slave.pronouns;
+
+  if (slave.drive > 95) return `${He}'s a nymphomaniac.`;
+  if (slave.drive > 80) return `${He}'s a sex addict.`;
+  if (slave.drive > 60) return `${He} has a powerful appetite for sex.`;
+  if (slave.drive > 40) return `${He} has a healthy sex drive.`;
+  if (slave.drive > 20) return `${He} has a weak sex drive.`;
+
+  return `${He}'s frigid, with little interest in sex.`;
+}
+
+function getMaleAttraction(slave: Slave): string {
+  const { He } = slave.pronouns;
+
+  if (slave.attraction.male < 95) {
+    if (slave.attraction.female < 95) return `${He}'s omnisexual, and is passionately attracted to nearly everyone.`;
+
+    return `${He}'s passionate about men,`;
+  }
+
+  if (slave.attraction.male < 85) return `${He} is aroused by most men,`;
+  if (slave.attraction.male < 65) return `${He} finds men attractive,`;
+  if (slave.attraction.male < 35) return `${He}'s indifferent to sex with men,`;
+  if (slave.attraction.male < 15) return `${He} considers men's bodies a turnoff,`;
+
+  return `${He} finds men sexually disgusting,`;
+}
+
+function getPassionate(slave: Slave): string {
+  if (slave.attraction.female > 85) return `${getMaleAttraction(slave)} but also thinks most women are hot, too.`;
+  if (slave.attraction.female > 65) return `${getMaleAttraction(slave)} but also likes women, too.`;
+  if (slave.attraction.female > 35) return `${getMaleAttraction(slave)} but is indifferent to women.`;
+  if (slave.attraction.female > 15) return `${getMaleAttraction(slave)} but is unenthusiastic about women.`;
+  if (slave.attraction.female > 5) return `${getMaleAttraction(slave)} but is turned off by women.`;
+
+  return `${getMaleAttraction(slave)} but is disgusted by the idea of intimacy with a woman.`;
+}
+
+function getArousedBy(slave: Slave): string {
+  if (slave.attraction.female > 95) return `${getMaleAttraction(slave)} but is most passionate about women.`;
+  if (slave.attraction.female > 85) return `${getMaleAttraction(slave)} and thinks most women are hot, too.`;
+  if (slave.attraction.female > 65) return `${getMaleAttraction(slave)} but is indifferent to women.`;
+  if (slave.attraction.female > 35) return `${getMaleAttraction(slave)} but is unenthusiastic about women.`;
+  if (slave.attraction.female > 15) return `${getMaleAttraction(slave)} and likes women too.`;
+  if (slave.attraction.female > 5) return `${getMaleAttraction(slave)} but is turned off by women.`;
+
+  return `${getMaleAttraction(slave)} but is disgusted by the idea of intimacy with a woman.`;
+}
+
+function getFindsAttractive(slave: Slave): string {
+  if (slave.attraction.female > 95) return `${getMaleAttraction(slave)} but is really passionate about women.`;
+  if (slave.attraction.female > 85) return `${getMaleAttraction(slave)} but likes women even more.`;
+  if (slave.attraction.female > 65) return `${getMaleAttraction(slave)} and likes women too.`;
+  if (slave.attraction.female > 35) return `${getMaleAttraction(slave)} but is indifferent to women.`;
+  if (slave.attraction.female > 15) return `${getMaleAttraction(slave)} but is unenthusiastic about women.`;
+  if (slave.attraction.female > 5) return `${getMaleAttraction(slave)} but is turned off by women.`;
+
+  return `${getMaleAttraction(slave)} but is disgusted by the idea of intimacy with a woman.`;
+}
+
+function getIsIndifferent(slave: Slave): string {
+  if (slave.attraction.female > 95) return `${getMaleAttraction(slave)} but is passionate about women.`;
+  if (slave.attraction.female > 85) return `${getMaleAttraction(slave)} strongly preferring women.`;
+  if (slave.attraction.female > 65) return `${getMaleAttraction(slave)} but is attracted to women.`;
+  if (slave.attraction.female > 35) return `${getMaleAttraction(slave)} and feels the same about women, too.`;
+  if (slave.attraction.female > 15) return `${getMaleAttraction(slave)} but is actually unenthusiastic about women.`;
+  if (slave.attraction.female > 5) return `${getMaleAttraction(slave)} but is turned off by women.`;
+
+  return `${getMaleAttraction(slave)} but is disgusted by the idea of intimacy with a woman.`;
+}
+
+function getTurnedOff(slave: Slave): string {
+  const { he } = slave.pronouns;
+
+  if (slave.attraction.female > 95) return `${getMaleAttraction(slave)} but is passionate about women.`;
+  if (slave.attraction.female > 85) return `${getMaleAttraction(slave)} strongly preferring women.`;
+  if (slave.attraction.female > 65) return `${getMaleAttraction(slave)} but is attracted to women.`;
+  if (slave.attraction.female > 35) return `${getMaleAttraction(slave)} and ${he} is indifferent to women.`;
+  if (slave.attraction.female > 15) return `${getMaleAttraction(slave)} and is unenthusiastic about women.`;
+
+  return `${getMaleAttraction(slave)} and is actually disgusted by women's.`;
+}
+
+function getFindsDisgusting(slave: Slave): string {
+  const { He, he } = slave.pronouns;
+
+  if (slave.attraction.female > 95) return `${getMaleAttraction(slave)} but is passionate about women.`;
+  if (slave.attraction.female > 85) return `${getMaleAttraction(slave)} strongly preferring women.`;
+  if (slave.attraction.female > 65) return `${getMaleAttraction(slave)} but is attracted to women.`;
+  if (slave.attraction.female > 35) return `${getMaleAttraction(slave)} and ${he} is indifferent to women.`;
+  if (slave.attraction.female > 15) return `${getMaleAttraction(slave)} and is unenthusiastic about women.`;
+  if (slave.attraction.female > 5) return `${getMaleAttraction(slave)} and has almost as much trouble with women.`;
+
+  return `${He} finds both men's and women's intimate areas quite repulsive, an unfortunate state of affairs!`;
+}
+
 function attraction(slave: Slave): string {
-  const { He, he, his } = slave.pronouns;
+  const { his } = slave.pronouns;
 
   const text: string[] = [];
 
   if (slave.attraction.known) {
-    if (slave.drive > 95) text.push(`${He}'s a nymphomaniac.`);
-    else if (slave.drive > 80) text.push(`${He}'s a sex addict.`);
-    else if (slave.drive > 60) text.push(`${He} has a powerful appetite for sex.`);
-    else if (slave.drive > 40) text.push(`${He} has a healthy sex drive.`);
-    else if (slave.drive > 20) text.push(`${He} has a weak sex drive.`);
-    else text.push(`${He}'s frigid, with little interest in sex.`);
+    text.push(getDrive(slave));
 
-    if (slave.attraction.male > 95) {
-      if (slave.attraction.female > 95) {
-        text.push(`${He}'s omnisexual, and is passionately attracted to nearly everyone.`);
-      } else {
-        text.push(`${He}'s passionate about men,`);
-
-        if (slave.attraction.female > 85) text.push('but also thinks most women are hot, too.');
-        else if (slave.attraction.female > 65) text.push('but also likes women, too.');
-        else if (slave.attraction.female > 35) text.push('but is indifferent to women.');
-        else if (slave.attraction.female > 15) text.push('but is unenthusiastic about women.');
-        else if (slave.attraction.female > 5) text.push('but is turned off by women.');
-        else text.push('but is disgusted by the idea of intimacy with a woman.');
-      }
-    } else if (slave.attraction.male > 85) {
-      text.push(`${He} is aroused by most men,`);
-
-      if (slave.attraction.female > 95) text.push('but is most passionate about women.');
-      else if (slave.attraction.female > 85) text.push('and thinks most women are hot, too.');
-      else if (slave.attraction.female > 65) text.push('but is indifferent to women.');
-      else if (slave.attraction.female > 35) text.push('but is unenthusiastic about women.');
-      else if (slave.attraction.female > 15) text.push('and likes women too.');
-      else if (slave.attraction.female > 5) text.push('but is turned off by women.');
-      else text.push('but is disgusted by the idea of intimacy with a woman.');
-    } else if (slave.attraction.male > 65) {
-      text.push(`${He} finds men attractive,`);
-
-      if (slave.attraction.female > 95) text.push('but is really passionate about women.');
-      else if (slave.attraction.female > 85) text.push('but likes women even more.');
-      else if (slave.attraction.female > 65) text.push('and likes women too.');
-      else if (slave.attraction.female > 35) text.push('but is indifferent to women.');
-      else if (slave.attraction.female > 15) text.push('but is unenthusiastic about women.');
-      else if (slave.attraction.female > 5) text.push('but is turned off by women.');
-      else text.push('but is disgusted by the idea of intimacy with a woman.');
-    } else if (slave.attraction.male > 35) {
-      text.push(`${He}'s indifferent to sex with men,`);
-
-      if (slave.attraction.female > 95) text.push('but is passionate about women.');
-      else if (slave.attraction.female > 85) text.push('strongly preferring women.');
-      else if (slave.attraction.female > 65) text.push('but is attracted to women.');
-      else if (slave.attraction.female > 35) text.push('and feels the same about women, too.');
-      else if (slave.attraction.female > 15) text.push('but is actually unenthusiastic about women.');
-      else if (slave.attraction.female > 5) text.push('but is turned off by women.');
-      else text.push('but is disgusted by the idea of intimacy with a woman.');
-    } else if (slave.attraction.male > 15) {
-      text.push(`${He} considers men's bodies a turnoff,`);
-
-      if (slave.attraction.female > 95) text.push('but is passionate about women.');
-      else if (slave.attraction.female > 85) text.push('strongly preferring women.');
-      else if (slave.attraction.female > 65) text.push('but is attracted to women.');
-      else if (slave.attraction.female > 35) text.push(`and ${he} is indifferent to women.`);
-      else if (slave.attraction.female > 15) text.push('and is unenthusiastic about women.');
-      else text.push('and is actually disgusted by women\'s.');
-    } else {
-      text.push(`${He} finds men sexually disgusting,`);
-
-      if (slave.attraction.female > 95) text.push('but is passionate about women.');
-      else if (slave.attraction.female > 85) text.push('strongly preferring women.');
-      else if (slave.attraction.female > 65) text.push('but is attracted to women.');
-      else if (slave.attraction.female > 35) text.push(`and ${he} is indifferent to women.`);
-      else if (slave.attraction.female > 15) text.push('and is unenthusiastic about women.');
-      else if (slave.attraction.female > 5) text.push('and has almost as much trouble with women.');
-      else text.push(`${He} finds both men's and women's intimate areas quite repulsive, an unfortunate state of affairs!`);
-    }
+    if (slave.attraction.male > 95) text.push(getPassionate(slave));
+    else if (slave.attraction.male > 85) text.push(getArousedBy(slave));
+    else if (slave.attraction.male > 65) text.push(getFindsAttractive(slave));
+    else if (slave.attraction.male > 35) text.push(getIsIndifferent(slave));
+    else if (slave.attraction.male > 15) text.push(getTurnedOff(slave));
+    else text.push(getFindsDisgusting(slave));
 
     return text.join(' ');
   }
