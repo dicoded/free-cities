7bb041beb4968f9f228c6867c2562eb4889f24a2
DCoded <dicoded@email.com>
2021-03-23 01:57:05 -0400

    Began refactoring eyes()

--- src/routes/slaves/View.svelte
+++ src/routes/slaves/View.svelte
@@ -77,6 +77,7 @@
 {skills(slave)}
 {family(slave)}
 {ears(slave)}
+{eyes(slave)}
 
 {armwear(slave)}
 {belly(slave)}
@@ -85,7 +86,6 @@
 {clothing(slave)}
 {counter(slave)}
 {crotch(slave)}
-{eyes(slave)}
 {face(slave)}
 {fetish(slave)}
 {flaws(slave)}
--- src/ts/util/slave/desc/eyes.ts
+++ src/ts/util/slave/desc/eyes.ts
@@ -1,110 +1,167 @@
 import Actor from '../../../classes/actor/Actor';
 
-export default function eyes(actor: Actor): string {
+function getActorBrilliant(actor: Actor): string {
   const { His, he, his } = actor.pronouns;
 
   const text: string[] = [];
 
-  if (actor.intelligence > 95) {
-    text.push(`${His} ${actor.eyes}-eyed gaze is incisive, quick, cunning;`);
+  text.push(`${His} ${actor.eyes}-eyed gaze is incisive, quick, cunning;`);
 
-    if (actor.intelligence + actor.education > 120) {
-      text.push(`with ${his} education, ${he} is so far beyond brilliant that ${he} is nearly peerless.`);
-    } else if (actor.education > 0) {
-      text.push(`${he} is both brilliant and`);
+  if (actor.intelligence + actor.education > 120) {
+    text.push(`with ${his} education, ${he} is so far beyond brilliant that ${he} is nearly peerless.`);
+  } else if (actor.education > 0) {
+    text.push(`${he} is both brilliant and`);
 
-      if (actor.education > 15) text.push('well');
+    if (actor.education > 15) text.push('well');
 
-      text.push('educated.');
-    } else {
-      text.push(`${he} is so brilliant that ${his} lack of education is unimportant.`);
-    }
-  } else if (actor.intelligence > 50) {
-    if (actor.intelligence + actor.education > 95) {
-      text.push(`${His} ${actor.eyes}-eyed gaze is incisive, quick, cunning; with ${his} education, ${he} can be
-      considered brilliant.`);
-    } else if (actor.education > 15) {
-      text.push(`${His} eyes are ${actor.eyes} are alive with intelligence; ${he} is both highly intelligent and `);
-
-      if (actor.education > 15) text.push('well');
-
-      text.push('educated.');
-    } else {
-      text.push(`${His} ${actor.eyes} eyes are alive with intelligence; ${he} is so highly intelligent that ${his}`);
-
-      if (actor.education > 0) text.push('meager');
-      else text.push('lack of');
-
-      text.push('education is unimportant.');
-    }
-  } else if (actor.intelligence > 15) {
-    text.push(`${His} ${actor.eyes} eyes are`);
-
-    if (actor.intelligence + actor.education > 50) {
-      text.push(`alive with intelligence; with ${his} education, ${he} can be considered highly intelligent.`);
-    } else if (actor.education > 0) {
-      text.push(`clever; ${he} is of above average intelligence and`);
-
-      if (actor.education > 15) text.push('well');
-
-      text.push('educated.');
-    } else {
-      text.push(`clever; ${he} is of above average intelligence despite being undereducated.`);
-    }
-  } else if (actor.intelligence >= -15) {
-    text.push(`${His} ${actor.eyes} eyes are`);
-
-    if (actor.intelligence + actor.education > 15) {
-      text.push(`clever; with ${his} education, ${he} can be considered of above average intelligence.`);
-    } else if (actor.education > 0) {
-      text.push(`alert; ${he} is of average intelligence due to being`);
-
-      if (actor.education > 15) text.push('well');
-
-      text.push('educated.');
-    } else {
-      text.push(`alert; ${he} is of average intelligence and is undereducated.`);
-    }
-  } else if (actor.intelligence >= -50) {
-    text.push(`${His} ${actor.eyes} eyes are`);
-
-    if (actor.intelligence + actor.education >= -15) {
-      text.push(`alert; with ${his} education, ${he} can be considered of average intelligence.`);
-    } else if (actor.education > 0) {
-      text.push(`dim; ${he} is of below average intelligence despite having been`);
-
-      if (actor.education > 15) text.push('thoroughly');
-
-      text.push('educated.');
-    } else {
-      text.push(`dim; ${he} is of below average intelligence and is poorly educated.`);
-    }
-  } else if (actor.intelligence >= -95) {
-    text.push(`${His} ${actor.eyes} eyes are`);
-
-    if (actor.intelligence + actor.education >= -50) {
-      text.push(`dim; even with ${his} education, ${he} can only be considered of below average intelligence.`);
-    } else if (actor.education > 0) {
-      text.push(`dull; ${he} is quite stupid despite having`);
-
-      if (actor.education > 15) text.push('an advanced');
-      else text.push('some');
-
-      text.push('education.');
-    } else {
-      text.push(`dull; ${he} is quite stupid and ignorant.`);
-    }
+    text.push('educated.');
   } else {
-    text.push(`${His} ${actor.eyes}-eyed gaze betrays near-total insensibility;`);
-
-    if (actor.intelligence + actor.education >= -95) {
-      text.push(`even with ${his} education, ${he} is still really stupid.`);
-    } else if (actor.education > 0) {
-      text.push(`${he} is a moron, yet somehow still remembers the basics of an education.`);
-    } else {
-      text.push(`${he} is a moron, and ignorant to boot.`);
-    }
+    text.push(`${he} is so brilliant that ${his} lack of education is unimportant.`);
   }
 
   return text.join(' ');
+}
+
+function getActorIntelligent(actor: Actor): string {
+  const { His, he, his } = actor.pronouns;
+
+  const text: string[] = [];
+
+  if (actor.intelligence + actor.education > 95) {
+    text.push(`${His} ${actor.eyes}-eyed gaze is incisive, quick, cunning; with ${his} education, ${he} can be
+      considered brilliant.`);
+  } else if (actor.education > 15) {
+    text.push(`${His} eyes are ${actor.eyes} are alive with intelligence; ${he} is both highly intelligent and `);
+
+    if (actor.education > 15) text.push('well');
+
+    text.push('educated.');
+  } else {
+    text.push(`${His} ${actor.eyes} eyes are alive with intelligence; ${he} is so highly intelligent that ${his}`);
+
+    if (actor.education > 0) text.push('meager');
+    else text.push('lack of');
+
+    text.push('education is unimportant.');
+  }
+
+  return text.join(' ');
+}
+
+function getActorClever(actor: Actor): string {
+  const { His, he, his } = actor.pronouns;
+
+  const text: string[] = [];
+
+  text.push(`${His} ${actor.eyes} eyes are`);
+
+  if (actor.intelligence + actor.education > 50) {
+    text.push(`alive with intelligence; with ${his} education, ${he} can be considered highly intelligent.`);
+  } else if (actor.education > 0) {
+    text.push(`clever; ${he} is of above average intelligence and`);
+
+    if (actor.education > 15) text.push('well');
+
+    text.push('educated.');
+  } else {
+    text.push(`clever; ${he} is of above average intelligence despite being undereducated.`);
+  }
+
+  return text.join(' ');
+}
+
+function getActorAverage(actor: Actor): string {
+  const { His, he, his } = actor.pronouns;
+
+  const text: string[] = [];
+
+  text.push(`${His} ${actor.eyes} eyes are`);
+
+  if (actor.intelligence + actor.education > 15) {
+    text.push(`clever; with ${his} education, ${he} can be considered of above average intelligence.`);
+  } else if (actor.education > 0) {
+    text.push(`alert; ${he} is of average intelligence due to being`);
+
+    if (actor.education > 15) text.push('well');
+
+    text.push('educated.');
+  } else {
+    text.push(`alert; ${he} is of average intelligence and is undereducated.`);
+  }
+
+  return text.join(' ');
+}
+
+function getActorBelowAverage(actor: Actor): string {
+  const { His, he, his } = actor.pronouns;
+
+  const text: string[] = [];
+
+  text.push(`${His} ${actor.eyes} eyes are`);
+
+  if (actor.intelligence + actor.education >= -15) {
+    text.push(`alert; with ${his} education, ${he} can be considered of average intelligence.`);
+  } else if (actor.education > 0) {
+    text.push(`dim; ${he} is of below average intelligence despite having been`);
+
+    if (actor.education > 15) text.push('thoroughly');
+
+    text.push('educated.');
+  } else {
+    text.push(`dim; ${he} is of below average intelligence and is poorly educated.`);
+  }
+
+  return text.join(' ');
+}
+
+function getActorStupid(actor: Actor): string {
+  const { His, he, his } = actor.pronouns;
+
+  const text: string[] = [];
+
+  text.push(`${His} ${actor.eyes} eyes are`);
+
+  if (actor.intelligence + actor.education >= -50) {
+    text.push(`dim; even with ${his} education, ${he} can only be considered of below average intelligence.`);
+  } else if (actor.education > 0) {
+    text.push(`dull; ${he} is quite stupid despite having`);
+
+    if (actor.education > 15) text.push('an advanced');
+    else text.push('some');
+
+    text.push('education.');
+  } else {
+    text.push(`dull; ${he} is quite stupid and ignorant.`);
+  }
+
+  return text.join(' ');
+}
+
+function getActorMoron(actor: Actor): string {
+  const { His, he, his } = actor.pronouns;
+
+  const text: string[] = [];
+
+  text.push(`${His} ${actor.eyes}-eyed gaze betrays near-total insensibility;`);
+
+  if (actor.intelligence + actor.education >= -95) {
+    text.push(`even with ${his} education, ${he} is still really stupid.`);
+  } else if (actor.education > 0) {
+    text.push(`${he} is a moron, yet somehow still remembers the basics of an education.`);
+  } else {
+    text.push(`${he} is a moron, and ignorant to boot.`);
+  }
+
+  return text.join(' ');
+}
+
+export default function eyes(actor: Actor): string {
+  if (actor.intelligence > 95) return getActorBrilliant(actor);
+  if (actor.intelligence > 50) return getActorIntelligent(actor);
+  if (actor.intelligence > 15) return getActorClever(actor);
+  if (actor.intelligence >= -15) return getActorAverage(actor);
+  if (actor.intelligence >= -50) return getActorBelowAverage(actor);
+  if (actor.intelligence >= -95) return getActorStupid(actor);
+
+  return getActorMoron(actor);
 }
