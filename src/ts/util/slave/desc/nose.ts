6f39d9eb8db53ffb8a590b2c9fa016cab747e06f
DCoded <dicoded@email.com>
2021-03-23 11:11:02 -0400

    Added nose)_

--- src/ts/util/slave/desc/nose.ts
+++ src/ts/util/slave/desc/nose.ts
@@ -1,7 +1,11 @@
 import Actor from '../../../classes/actor/Actor';
 
 export default function nose(actor: Actor): string {
+  const { He, his } = actor.pronouns;
+
   const text: string[] = [];
+
+  text.push(`${He} has no sense of smell, but this isn't immediately obvious just by looking at ${his} nose.`);
 
   return text.join(' ');
 }
