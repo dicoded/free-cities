09ce424e260599984d3fa4f605a4e17b8500079a
DCoded <dicoded@email.com>
2021-04-10 13:11:51 -0400

    Fixed test paths

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,15 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    '@app/(.*)': '<rootDir>/src/$1',
+    '@components/(.*)': '<rootDir>/src/components/$1',
+    '@data/(.*)': '<rootDir>/src/data/$1',
+    '@routes/(.*)': '<rootDir>/src/routes/$1',
+    '@stores/(.*)': '<rootDir>/src/stores/$1',
+    '@test/(.*)': '<rootDir>/src/test/$1',
+    '@classes/(.*)': '<rootDir>/src/ts/classes/$1',
+    '@util/(.*)': '<rootDir>/src/ts/util/$1',
+    '@extensions/(.*)': '<rootDir>/src/ts/util/extensions/$1'
+  }
 };
