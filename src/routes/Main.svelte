1ae9b5faa8350cbf0b3cda9e0000549461d0a868
DCoded <dicoded@email.com>
2021-04-11 14:56:59 -0400

    Added basic arcology layout

--- src/routes/Main.svelte
+++ src/routes/Main.svelte
@@ -5,6 +5,7 @@
 	import { week } from '@stores/global.store';
 
 	import Facilities from './facilities/Facilities.svelte';
+	import Layout from './facilities/layout.svelte';
 	import Slaves from './actors/Slaves.svelte';
 
 	import Header from '@components/ui/Header.svelte';
@@ -27,6 +28,6 @@
 
 <Body>
 	<Header>{$arcology.name}</Header>
-	<Facilities />
+	<Layout />
 	<Slaves />
 </Body>
---
+++ src/routes/facilities/layout.svelte
@@ -0,0 +1,87 @@
+<script lang="ts">
+	import Link from '@components/ui/Link.svelte';
+</script>
+
+<div class="mt-8">
+	<div class="row">
+		<div class="cell penthouse">
+			<Link to="">
+				Penthouse
+			</Link>
+		</div>
+	</div>
+	<div class="row">
+		<div class="cell">
+			<Link to=""/>
+		</div>
+		<div class="cell">
+			<Link to=""/>
+		</div>
+	</div>
+	<div class="row">
+		<div class="cell">
+			<Link to=""/>
+		</div>
+		<div class="cell">
+			<Link to=""/>
+		</div>
+	</div>
+	<div class="row">
+		<div class="cell">
+			<Link to=""/>
+		</div>
+		<div class="cell">
+			<Link to=""/>
+		</div>
+		<div class="cell">
+			<Link to=""/>
+		</div>
+	</div>
+	<div class="row">
+		<div class="cell">
+			<Link to=""/>
+		</div>
+		<div class="cell">
+			<Link to=""/>
+		</div>
+		<div class="cell">
+			<Link to=""/>
+		</div>
+		<div class="cell">
+			<Link to=""/>
+		</div>
+	</div>
+	<div class="row">
+		<div class="cell">
+			<Link to=""/>
+		</div>
+		<div class="cell">
+			<Link to=""/>
+		</div>
+		<div class="cell">
+			<Link to=""/>
+		</div>
+		<div class="cell">
+			<Link to=""/>
+		</div>
+	</div>
+</div>
+
+<style>
+	.row {
+		@apply flex;
+		@apply flex-row;
+		@apply justify-center;
+		@apply w-full;
+	}
+
+	.cell {
+		@apply flex;
+		@apply h-16;
+		@apply w-1/4;
+	}
+
+	.penthouse {
+		@apply w-1/2;
+	}
+</style>
