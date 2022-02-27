35d520deafd81b863317d59dee16ba146eea665a
Dan <dcoded@live.com>
2022-02-27 11:56:32 -0500

    Typing fixes

--- src/routes/intro/character/Overview.svelte
+++ src/routes/intro/character/Overview.svelte
@@ -1,18 +1,21 @@
 <script lang="ts">
+	import { EyeColor } from '@app/ts/util/color';
+
 	import Response from '@components/ui/Response.svelte';
 	import Section from '@components/ui/Section.svelte';
 
 	import PC from '@stores/player.store';
+	import Markings from '@classes/body/nonphysical/skin/Markings';
 
 	let acquisition: string;
 	let age: number;
 	let birthday: string;
 	let career: string;
-	let eyes: string;
+	let eyes: EyeColor | null;
 	let face: string;
 	let name: string;
 	let hair: string;
-	let markings: string;
+	let markings: Markings;
 	let nationality: string;
 	let race: string;
 	let sex: string;
@@ -37,6 +40,14 @@
 
 <Section>
 	<Response>
-		My name is <span class="rb">{name}</span>. I'm a <span class="rb">{age}</span>-year-old <span class="rb">{race} {nationality} {sex}</span> with a <span class="rb">{skin}</span> complexion, and I was born on <span class="rb">{birthday}</span>. I was once <span class="rb">{career}</span>, and I acquired my arcology through <span class="rb">{acquisition}</span>. I have <span class="rb">{eyes}</span> eyes and <span class="rb">{hair}</span> hair, and my face is <span class="rb">{face}</span> in shape. <span class="rb">{markings}</span>.
+		My name is <span class="rb">{name}</span>. I'm a <span class="rb">{age}</span>-year-old <span class="rb">{race} {nationality} {sex}</span> with a <span class="rb">{skin}</span> complexion, and I was born on <span class="rb">{birthday}</span>. I was once <span class="rb">{career}</span>, and I acquired my arcology through <span class="rb">{acquisition}</span>. I have <span class="rb">{eyes}</span> eyes and <span class="rb">{hair}</span> hair, and my face is <span class="rb">{face}</span> in shape.  {#if markings.arms}
+			<span class="rb">{markings.arms}</span>.
+			<span class="rb">{markings.belly}</span>.
+			<span class="rb">{markings.chest}</span>.
+			<span class="rb">{markings.crotch}</span>.
+			<span class="rb">{markings.face}</span>.
+			<span class="rb">{markings.legs}</span>.
+			<span class="rb">{markings.shoulders}</span>.
+		{/if}
 	</Response>
 </Section>
